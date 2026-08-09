4ea8b04d587c421a62e29825289086e5e53d7a68
ofixe98 <miguelaavieira1998@gmail.com>
2026-08-09 18:16:49 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,17 @@
-const CACHE = 'delegado-afl-v10-9-offline';
-const APP = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
+const CACHE='delegado-afl-v11-android';
+const ASSETS=[
+ './','./index.html','./manifest.webmanifest',
+ './icons/icon-192.png','./icons/icon-512.png',
+ './docs/RPO-26-27-FUTEBOL-11-SENIORES_v11.pdf',
+ './docs/AGE-20260627-AVISO-CONVOCATORIO-SIGNED-VF-ERRATA.pdf',
+ './docs/CO-001-26-27-COMUNICADO-OFICIAL-1.pdf'
 ];
-const DOCS = [
-  './docs/RPO-26-27-FUTEBOL-11-SENIORES_v11.pdf',
-  './docs/AGE-20260627-AVISO-CONVOCATORIO-SIGNED-VF-ERRATA.pdf',
-  './docs/CO-001-26-27-COMUNICADO-OFICIAL-1.pdf'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(APP.concat(DOCS)))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        if (response && response.ok) {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        }
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+   if(r&&r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+   return r;
+ }).catch(()=>caches.match('./index.html'))));
 });
