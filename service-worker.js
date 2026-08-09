261305b21b01fcdf10a149e832a554aa62581da6
ofixe98 <miguelaavieira1998@gmail.com>
2026-08-09 18:59:32 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE='delegado-afl-v11-3';
+const ASSETS=[
+ './','./index.html','./manifest.webmanifest',
+ './icons/icon-192.png','./icons/icon-512.png',
+ './docs/RPO-26-27-FUTEBOL-11-SENIORES_v11.pdf',
+ './docs/AGE-20260627-AVISO-CONVOCATORIO-SIGNED-VF-ERRATA.pdf',
+ './docs/CO-001-26-27-COMUNICADO-OFICIAL-1.pdf'
+];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+   if(r&&r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+   return r;
+ }).catch(()=>caches.match('./index.html'))));
+});
